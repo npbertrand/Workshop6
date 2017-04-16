da9e187efa67b27de0653d60b98776cd6ee674f3
Nicholas Bertrand <nbertrand@umass.edu>
2017-04-16 13:47:18 -0400

    express

---
+++ server/src/server.js
@@ -0,0 +1,33 @@
+// Imports the express Node module.
+var express = require('express');
+// Creates an Express server.
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.text());
+
+
+
+// Defines what happens when it receives the `GET /` request
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+// Starts the server on port 3000!
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+var reverseString = require('./util');
+
+
+
+// Handle POST /reverse [data]
+app.post('/reverse', function (req, res) {
+  if (typeof(req.body) === 'string') {
+    var reversed = reverseString.reverseString(req.body);
+    res.send(reversed);
+  } else {
+    res.status(400).end();
+  }
+});
