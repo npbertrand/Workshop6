d0acf93740cfd0f777c1e376b4b374dc39c653b3
Nicholas Bertrand <nbertrand@umass.edu>
2017-04-17 12:57:14 -0400

    almostDone

--- client/app/server.js
+++ client/app/server.js
@@ -73,37 +73,31 @@ export function postComment(feedItemId, author, contents, cb) {
  * Updates a feed item's likeCounter by adding the user to the likeCounter.
  * Provides an updated likeCounter in the response.
  */
-export function likeFeedItem(feedItemId, userId, cb) {
-  var feedItem = readDocument('feedItems', feedItemId);
-  // Normally, we would check if the user already liked this comment.
-  // But we will not do that in this mock server.
-  // ('push' modifies the array by adding userId to the end)
-  feedItem.likeCounter.push(userId);
-  writeDocument('feedItems', feedItem);
-  // Return a resolved version of the likeCounter
-  emulateServerReturn(feedItem.likeCounter.map((userId) => readDocument('users', userId)), cb);
-}
+ /**
+  * Updates a feed item's likeCounter by adding the user to the likeCounter.
+  * Provides an updated likeCounter in the response.
+  */
+ export function likeFeedItem(feedItemId, userId, cb) {
+   sendXHR('PUT', '/feeditem/' + feedItemId + '/likelist/' + userId,
+           undefined, (xhr) => {
+     cb(JSON.parse(xhr.responseText));
+   });
+ }
 
 /**
  * Updates a feed item's likeCounter by removing the user from the likeCounter.
  * Provides an updated likeCounter in the response.
  */
-export function unlikeFeedItem(feedItemId, userId, cb) {
-  var feedItem = readDocument('feedItems', feedItemId);
-  // Find the array index that contains the user's ID.
-  // (We didn't *resolve* the FeedItem object, so it is just an array of user IDs)
-  var userIndex = feedItem.likeCounter.indexOf(userId);
-  // -1 means the user is *not* in the likeCounter, so we can simply avoid updating
-  // anything if that is the case: the user already doesn't like the item.
-  if (userIndex !== -1) {
-    // 'splice' removes items from an array. This removes 1 element starting from userIndex.
-    feedItem.likeCounter.splice(userIndex, 1);
-    writeDocument('feedItems', feedItem);
-  }
-  // Return a resolved version of the likeCounter
-  emulateServerReturn(feedItem.likeCounter.map((userId) => readDocument('users', userId)), cb);
-}
-
+ /**
+  * Updates a feed item's likeCounter by removing the user
+  * from the likeCounter. Provides an updated likeCounter
+  * in the response.
+  */
+ export function unlikeFeedItem(feedItemId, userId, cb) {
+   sendXHR('DELETE', '/feeditem/' + feedItemId + '/likelist/' + userId,undefined, (xhr) => {
+     cb(JSON.parse(xhr.responseText));
+   });
+ }
 /**
  * Adds a 'like' to a comment.
  */
@@ -135,37 +129,17 @@ export function unlikeComment(feedItemId, commentIdx, userId, cb) {
  * Updates the text in a feed item (assumes a status update)
  */
 export function updateFeedItemText(feedItemId, newContent, cb) {
-  var feedItem = readDocument('feedItems', feedItemId);
-  // Update text content of update.
-  feedItem.contents.contents = newContent;
-  writeDocument('feedItems', feedItem);
-  emulateServerReturn(getFeedItemSync(feedItemId), cb);
+  sendXHR('PUT', '/feeditem/' + feedItemId + '/content', newContent, (xhr) => {
+    cb(JSON.parse(xhr.responseText));
+  });
 }
-
 /**
  * Deletes a feed item.
  */
 export function deleteFeedItem(feedItemId, cb) {
-  // Assumption: The current user authored this feed item.
-  deleteDocument('feedItems', feedItemId);
-  // Remove references to this feed item from all other feeds.
-  var feeds = getCollection('feeds');
-  var feedIds = Object.keys(feeds);
-  feedIds.forEach((feedId) => {
-    var feed = feeds[feedId];
-    var itemIdx = feed.contents.indexOf(feedItemId);
-    if (itemIdx !== -1) {
-      // Splice out of array.
-      feed.contents.splice(itemIdx, 1);
-      // Update feed.
-      writeDocument('feeds', feed);
-    }
+  sendXHR('DELETE', '/feeditem/' + feedItemId, undefined, () => {
+    cb();
   });
-
-  // Return nothing. The return just tells the client that
-  // the server has acknowledged the request, and that it has
-  // been a success.
-  emulateServerReturn(null, cb);
 }
 
 /**
